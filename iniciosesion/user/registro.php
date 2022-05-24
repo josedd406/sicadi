bc6eb2c9fdfc6c44f4531f48341f488c4c700198
josedd406 <josedd40617@gmail.com>
2022-05-24 13:51:49 -0500

    Verificacion de Correo Repetido iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- iniciosesion/user/registro.php
+++ iniciosesion/user/registro.php
@@ -6,6 +6,20 @@ $message = '';
 if (!empty($_POST['nombre']) && !empty($_POST['telefono']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'] && !empty($_POST['c_password']))) {
 	$pw1 = $_POST['password'];
 	$pw2 = $_POST['c_password'];
+  $email = $_POST['email'];
+
+  $correo_duplicado = "";
+  $verificar_correo = "SELECT * FROM users WHERE email='$email'";
+    foreach ($conn->query($verificar_correo) as $row) {
+        //print $row['email'] . "\t";
+        $correo_duplicado = $row['email'];
+
+    }
+    if ($correo_duplicado!=""){
+      echo "Este correo ya esta registrado";
+    } else {
+      
+
   if($pw1 == $pw2){
 	$sql = "INSERT INTO users (nombre, telefono, username, email, password, privilegios) VALUES (:nombre, :telefono, :username, :email, :password, :privilegios)";
 	$stmt = $conn->prepare($sql);
