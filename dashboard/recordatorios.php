30c82170e14c08a79c020a9d48f5cf851088cbd8
josedd406 <josedd40617@gmail.com>
2022-06-27 14:30:22 -0500

    Api Recordatorios

---
+++ dashboard/recordatorios.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+
+require '../iniciosesion/user/database.php';
+
+if (isset($_SESSION['user_id'])) {
+  $records = $conn->prepare('SELECT * FROM users WHERE id = :id');
+  $records->bindParam(':id', $_SESSION['user_id']);
+  $records->execute();
+  $results = $records->fetch(PDO::FETCH_ASSOC);
+
+  $user = null;
+
+  if (count($results) > 0) {
+    $user = $results;
+  }
+  //var_dump($results);
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Administrador</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+  </head>
+  <body>
+    <div id="main">
+      <header>
+        <div id="logo">
+          <div id="logo_text">
+            <h1><a href="../index.php">SICADI<span class="logo_colour"> Transformetal y Reparaciones </span></a></h1>
+            <h2>Administrador</h2>
+        </div>
+        </div>
+        <nav>
+          <ul class="sf-menu" id="nav">
+            <li><a href="../index.php">Inicio</a></li>
+            <li><a href="../informacion/informacion.php">Quienes somos</a></li>
+            <li><a href="../galeria/galeria.php">Galeria</a></li>
+            <li><a href="../citas/citas.php">Citas</a></li>
+
+            <li><a href="../contacto/contacto.php">Contacto</a></li>
+            <?php if(!empty($user)): ?>
+            <li><a><?= $user['email'] ?></a>
+              <ul>
+                <li><a href="#">Mis pedidos</a></li>
+                <li><a href="#">Mis presupestos</a></li>
+                <?php
+                if($user['privilegios'] == "admin"){
+                ?>
+                <li><a href="../dashboard/admin.php">Administrar</a></li>
+                <?php
+              }
+                 ?>
+                <li><a href="../iniciosesion/user/logout.php">Cerrar Sesion</a></li>
+              </li>
+            </ul>
+            <?php else: ?>
+            <li><a href="../iniciosesion/user/login.php">Iniciar sesión</a></li>
+          <?php endif; ?>
+          </ul>
+        </nav>
+      </header>
+	  <div id="site_content">
+		<div class="content">
+		  <div id="menu_admin">
+			<div id="logo_admin">
+			  <p class="texto"><a href="admin.php"><?= $user['username'] ?></a></p>
+			</div>
+			<div id="contenedor_primero" class="contenedor">
+			  <div id="logo_interno">
+
+			  </div>
+			  <p class="texto"><a href="pusuarios.php">Usuarios</a></p>
+			</div>
+      <div class="contenedor">
+			  <div id="logo_interno">
+
+			  </div>
+        <p class="texto"><a href="recordatorios.php">Recordatorio</a></p>
+			</div>
+			<div class="contenedor">
+			  <div id="logo_interno">
+
+			  </div>
+        <p class="texto"><a href="pcitas.php">Citas</a></p>
+			</div>
+			<div class="contenedor">
+			  <div id="logo_interno">
+
+			  </div>
+			  <p class="texto"><a href="../dashboard/pgaleria.php">Galeria</a></p>
+			</div>
+		  </div>
+		  <div id="contenido">
+			<h2>Recordatorios</h2>
+        <!-- Principio del widget integrado de Calendly -->
+        <div class="calendly-inline-widget"
+          data-url="https://calendly.com/cbtaguilar16?hide_landing_page_details=1&hide_gdpr_banner=1"
+          style="min-width:220px;height:330px;"></div>
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+        <!-- Final del widget integrado de Calendly -->
+			</div>
+		</div>
+	  </div>
+    </div>
+  </body>
+</html>
